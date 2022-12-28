0fdd5c29610fd77437baf63a8936b6e2c2ae7f0d
saierukala <sai.erukala1508@gmail.com>
2022-12-28 09:29:09 +0000

    codintional using element varialbles

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import {Component} from 'react'
+
+import Welcome from './components/Welcome'
+
+import './App.css'
+
+class App extends Component {
+  state = {
+    isLoggedIn: true,
+  }
+
+  render() {
+    const {isLoggedIn} = this.state
+    let authButton
+    if (isLoggedIn === true) {
+      authButton = <button>Logout</button>
+    } else {
+      authButton = <button>Login</button>
+    }
+    return (
+      <div className="container">
+        <Welcome greeting="Hello" name="User" />
+        {authButton}
+      </div>
+    )
+  }
+}
+
+export default App
