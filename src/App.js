89bd26f0db675ccf843a40fad82ed62788e5e6f5
saierukala <sai.erukala1508@gmail.com>
2022-12-28 09:33:56 +0000

    coditional using if..else satement

--- src/App.js
+++ src/App.js
@@ -9,18 +9,19 @@ class App extends Component {
     isLoggedIn: true,
   }
 
-  render() {
+  renderAuthButton = () => {
     const {isLoggedIn} = this.state
-    let authButton
     if (isLoggedIn === true) {
-      authButton = <button>Logout</button>
-    } else {
-      authButton = <button>Login</button>
+      return <button>Logout</button>
     }
+    return <button>Login</button>
+  }
+
+  render() {
     return (
       <div className="container">
         <Welcome greeting="Hello" name="User" />
-        {authButton}
+        {this.renderAuthButton()}
       </div>
     )
   }
